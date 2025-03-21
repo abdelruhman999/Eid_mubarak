ca0bbb1bc37fcd5a5aee76f2062145b0a90641db
Hesham Moawad <heshammoawad120120@yahoo.com>
2025-03-21 10:26:03 +0200

    feat: add API constants and request handler for improved data fetching

---
+++ src/api/index.ts
@@ -0,0 +1,37 @@
+import { BASE_URL } from "./constants";
+
+
+
+export interface sendRequestKwargs {
+    url:string,
+    method?:"GET"|"POST"|"PUT"|"DELETE",
+    params?:Record<string,string>,
+    data?:BodyInit|null,
+    headers?:object,
+    cache?:RequestCache,
+    next?:{
+        revalidate:number
+    }
+}
+
+
+export const sendRequest = async <T>({url,method,params,data , headers , cache ,  next}:sendRequestKwargs):Promise<T>=>{
+    const response = await fetch(`${BASE_URL}${url}${params ? "?" + new URLSearchParams(params).toString():""}` , {
+        method: method,
+        body: data , 
+        credentials: 'include', // include cookies in the request
+        headers: {
+            "Content-Type": "application/json",
+            ...headers,
+        },
+        cache: cache,
+        ...next
+        
+      });
+
+    if(!response.ok){
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+}
